44d80cbda5648ea2a5f065b4d8feaef96a2ce2ac
gabriel <greis826@gmail.com>
2024-11-28 12:56:52 -0300

    importação das classes principais

---
+++ application/livroService.py
@@ -0,0 +1,7 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from model.entities.livro import Livro
+from model.dao.daoFactory import DaoFactory
+from model.dao.livroDao import LivroDao
+
