61f5a382f0798d1bed9cef9242935d77ce929d3a
gabriel <greis826@gmail.com>
2024-11-28 13:00:46 -0300

    criação do metodo para conexão com banco com classe livrodaSL3

--- model/dao/daoFactory.py
+++ model/dao/daoFactory.py
@@ -1,6 +1,10 @@
 from db.db import DB
 from model.dao.impl.alunoDaoSL3 import AlunoDaoSL3
+from model.dao.impl.livroDaoSL3 import LivroDaoSL3
 
 class DaoFactory:
     def createAlunoDao():
         return  AlunoDaoSL3(DB.getConn())
+    
+    def createLivroDao():
+        return LivroDaoSL3(DB.getConn())
