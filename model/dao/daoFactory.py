0263708f43276604a2432f3ded06303971a584f6
gabriel <greis826@gmail.com>
2024-11-29 14:15:02 -0300

    criação do metodo para instaciar a classe de conexão com a tabela biblioteca do banco de dados

--- model/dao/daoFactory.py
+++ model/dao/daoFactory.py
@@ -1,6 +1,7 @@
 from db.db import DB
 from model.dao.impl.alunoDaoSL3 import AlunoDaoSL3
 from model.dao.impl.livroDaoSL3 import LivroDaoSL3
+from model.dao.impl.bibliotecaDaoSL3 import BibliotecaDaoSL3
 
 class DaoFactory:
     def createAlunoDao():
@@ -8,3 +9,6 @@ class DaoFactory:
     
     def createLivroDao():
         return LivroDaoSL3(DB.getConn())
+    
+    def createBibliotecaDao():
+        return BibliotecaDaoSL3(DB.getConn)
