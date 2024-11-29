3566a5fd11f1389d9894d6d39755ec3375a48c1f
gabriel <greis826@gmail.com>
2024-11-29 14:10:43 -0300

    criação da base da classe

--- model/dao/impl/bibliotecaDaoSL3.py
+++ model/dao/impl/bibliotecaDaoSL3.py
@@ -1,1 +1,32 @@
+from typing import List
+import sqlite3
 
+from model.entities.biblioteca import Biblioteca
+from model.dao.bibliotecaDao import BibliotecaDao
+from db.db import DB
+from db.dbException import DbException
+
+class BibliotecaDaoSL3(BibliotecaDao):
+    
+    def __init__(self,conn):
+        super().__init__()
+        self.conn= conn
+
+    def insert(self,biblioteca: Biblioteca):
+        pass
+
+    
+    def update(self,biblioteca: Biblioteca):
+        pass
+
+   
+    def deleteById(self,id: int):
+        pass
+
+   
+    def findById(self,id: int):
+        pass
+
+   
+    def findAll(self,biblioteca: List[Biblioteca]):
+        pass
