8fa08529d71072ee444acc2928de79ce2f78f71f
gabriel <greis826@gmail.com>
2025-02-05 15:59:24 -0300

    criação de meteodo para represerntação do objeto

--- model/entities/avaliacao.py
+++ model/entities/avaliacao.py
@@ -1,18 +1,32 @@
-from livro import Livro
-from aluno import Aluno
-from reserva import Reserva
-from avaliacao import Avaliacao
+from model.entities.livro import Livro
+from model.entities.aluno import Aluno
+from model.entities.reserva import Reserva
 
 from typing import List
 
 class Avaliacao:
-    def __init__(self, id, nota, aluno: Aluno, livro: Livro, reserva: Reserva):
+    def __init__(self, id: int, nota: int, aluno: Aluno, livro: Livro, reserva: Reserva):
         self.id=id
         self.nota=nota
         self.aluno=aluno
         self.livro=livro
         self.reserva=reserva
 
-    def media(self, notas: List[Avaliacao]):
-        total= sum(nota.nota for nota in notas)
-        return total/len(notas)
+    def __iter__(self):
+        yield self.id
+        yield self.nota
+        yield self.aluno.nome
+        yield self.livro.titutlo
+        yield self.reserva.id
+
+    def __str__(self):
+        return str(tuple(self.__iter__())) 
+        #f'({self.id}, {self.nota}, {self.aluno.nome}, {self.livro.titutlo}, {self.reserva.id})'
+    
+    def __repr__(self):
+        return self.__str__()
+    
+
+    # def media(self, notas: List[Avaliacao]):
+    #     total= sum(nota.nota for nota in notas)
+    #     return total/len(notas)
