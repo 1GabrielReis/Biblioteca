c304e4de1384f1d15e76cbf3e55ff26a1ad76211
gabriel <greis826@gmail.com>
2025-02-06 22:22:55 -0300

    ajuste dos metodos em relação a clsse abstrata

--- model/dao/impl/avaliacaoDaoSL3.py
+++ model/dao/impl/avaliacaoDaoSL3.py
@@ -15,4 +15,27 @@ class AvaliacaoDaoSL3(AvaliacaoDao):
     def __init__(self, conn):
         super().__init__()
         self.conn= conn
-        
+
+    def insert(self,avaliacao: Avaliacao):
+        pass
+
+    def update(self,avaliacao: Avaliacao):
+        pass
+
+    def deleteById(self,id: int):
+        pass
+
+    def findById(self,id: int):
+        pass
+
+    def findAll(self) -> List[Avaliacao]:
+        pass
+
+    def findByAluno(self, avaliacao) -> List[Avaliacao]:
+        pass
+
+    def findByLivro(self, avaliacao) -> List[Avaliacao]:
+        pass
+
+    def findByReserva(self, avaliacao) -> List[Avaliacao]:
+        pass
