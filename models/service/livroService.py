92d248c7bd820d69dde85955e05a39eb6541ae2d
gabriel <greis826@gmail.com>
2025-05-10 15:11:53 -0300

    inserção dos try except nos metodos e inserçaõ do instanceObject

--- models/service/livroService.py
+++ models/service/livroService.py
@@ -1,9 +1,12 @@
 from typing import List
 
-from model.entities.livro import Livro
-from model.dao.livroDao import LivroDao
+from ..model.entities.livro import Livro
+from ..model.dao.livroDao import LivroDao
+from ..model.dao.daoFactory import DaoFactory
 
-from model.dao.daoFactory import DaoFactory
+from ..schemas.livro_schema import Livro_Schema
+
+from ..service.serviceException import ServiceException
 
 class LivroService(LivroDao):
     def __init__(self):
@@ -11,16 +14,37 @@ class LivroService(LivroDao):
         self.livroDao= DaoFactory.createLivroDao()
 
     def insert(self,livro: Livro):
-        return self.livroDao.insert(livro)
+        try:
+            return self.livroDao.insert(livro)
+        except Exception as e:
+            raise ServiceException(f"Erro ao inserir livro.\nDetalhes: {e}")
 
     def update(self,livro: Livro):
-        return self.livroDao.update(livro)
+        try:
+            return self.livroDao.update(livro)
+        except Exception as e:
+            raise ServiceException(f"Erro ao atualizar livro.\nDetalhes: {e}")
 
     def deleteById(self,id: int):
-        return self.livroDao.deleteById(id)
+        try:
+            return self.livroDao.deleteById(id)
+        except Exception as e:
+            raise ServiceException(f"Erro ao deletar livro com ID {id}.\nDetalhes: {e}")
 
     def findById(self,id: int):
-        return self.livroDao.findById(id)
+        try:
+            return self.livroDao.findById(id)
+        except Exception as e:
+            raise ServiceException(f"Erro ao buscar livro com ID {id}.\nDetalhes: {e}")
 
     def findAll(self) -> List[Livro]:
-        return self.livroDao.findAll()
+        try:
+            return self.livroDao.findAll()
+        except Exception as e:
+            raise ServiceException(f"Erro ao buscar todos os livros. \nDetalhes: {e}")
+        
+    def instanceObject(self, livro: Livro_Schema, id= None) -> Livro:
+        try:
+            return livro(id= id, titulo= livro.titulo, autor= livro.autor, editora= livro.editora)
+        except Exception as e:
+            raise ServiceException(f"Erro ao instância livro \nDetalhes: {e}")
