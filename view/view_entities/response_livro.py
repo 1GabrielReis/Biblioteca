73db122ac912a53db75e794ade2357da7719c435
gabriel <greis826@gmail.com>
2025-05-10 23:13:33 -0300

    logicas dos metodos

--- view/view_entities/response_livro.py
+++ view/view_entities/response_livro.py
@@ -3,7 +3,19 @@ from ..response_base import Response_base
 class Response_livro(Response_base):
 
     def format(self,livro):
-        pass
+        return {
+            "status": "sucesso",
+            "dados": {
+                "id": livro.id,
+                "titulo": livro.titulo,
+                "autor": livro.autor,
+                "editora": livro.editora
+            }
+        }
 
-    def format_list(self,livro):
-        pass
+    def format_list(self,livros):
+        return {
+            "status": "sucesso",
+            "quantidade": len(livros),
+            "dados": [self.format(livro)["dados"] for livro in livros] if livros else []
+        }
