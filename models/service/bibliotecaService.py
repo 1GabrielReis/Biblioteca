c2d2f53e125c062c5afb6e34e89a7734b2683e7a
gabriel <greis826@gmail.com>
2025-05-19 20:49:37 -0300

    ajuste no metodo findByAluno

--- models/service/bibliotecaService.py
+++ models/service/bibliotecaService.py
@@ -3,7 +3,6 @@ from typing import List
 from ..model.entities.aluno import Aluno
 from ..model.entities.biblioteca import Biblioteca
 from ..model.dao.bibliotecaDao import BibliotecaDao
-from ..schemas.aluno_schema import Aluno_Schema
 from ..schemas.biblioteca_schema import Biblioteca_schema
 
 from ..model.dao.daoFactory import DaoFactory
@@ -45,18 +44,12 @@ class  BibliotecaService(BibliotecaDao):
         except Exception as e:
             raise ServiceException(f"Erro ao buscar todos as avalições da biblioteca. \nDetalhes: {e}")
 
-    def findByAluno(self, aluno: Aluno) -> List[Aluno]:
+    def findByAluno(self, id: int) -> List[Aluno]:
         try:
-            return self.bibliotecaDao.findByAluno(aluno)
+            return self.bibliotecaDao.findByAluno(id)
         except Exception as e:
             raise ServiceException(f"Erro ao buscar todos as avalições da biblioteca por aluno \nDetalhes: {e}")
-        
-    def instance_aluno(self, aluno: Aluno_Schema, id= None) -> Aluno:
-        try:
-            return Aluno(id = id, nome= aluno.nome, sobrenome= aluno.sobrenome)
-        except Exception as e:
-            raise ServiceException(f"Erro ao instanciar aluno \nDetalhes: {e}")
-        
+            
     def instance_biblioteca(self, biblioteca: Biblioteca_schema, id= None) -> Biblioteca:
         try:
             return Biblioteca(id= id, nota= biblioteca.nota, aluno=Aluno(biblioteca.aluno_id, None, None))
