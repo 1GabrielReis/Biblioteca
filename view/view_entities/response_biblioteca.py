96aaaec860fe7acbea8c760878ef33fa41769fd4
gabriel <greis826@gmail.com>
2025-05-19 15:28:34 -0300

    criaçação do modelo json para biblioteca

--- view/view_entities/response_biblioteca.py
+++ view/view_entities/response_biblioteca.py
@@ -1,10 +1,28 @@
 from ..response_base import Response_base
+import json
 
 class Response_biblioteca(Response_base):
     
-    def format(data):
-        pass
+    def format(self, biblioteca):
+        return {
+            "status": "sucesso",
+            "dados": {
+                "id": biblioteca.id,
+                "nota": biblioteca.nota,
+                "aluno": {
+                    "id":biblioteca.aluno.id,
+                    "nome":biblioteca.aluno.nome,
+                    "sobrenome": biblioteca.aluno.sobrenome
+                }
+            }
+        }
+        
 
     
-    def format_list(data):
-        pass
+    def format_list(self, biblioteca_notas):
+        return {
+            "status": "sucesso",
+            "quantidade": len(biblioteca_notas),
+            "dados": [self.format(biblioteca)["dados"] for biblioteca in biblioteca_notas] if biblioteca_notas else []
+        }
+        
