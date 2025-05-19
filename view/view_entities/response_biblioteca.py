cab641db6355825d8956f3749ce6a97dc0e7245d
gabriel <greis826@gmail.com>
2025-05-19 20:52:09 -0300

    metodo para receber o findByAluno

--- view/view_entities/response_biblioteca.py
+++ view/view_entities/response_biblioteca.py
@@ -17,11 +17,39 @@ class Response_biblioteca(Response_base):
         }
         
 
-    
     def format_list(self, biblioteca_notas):
         return {
             "status": "sucesso",
             "quantidade": len(biblioteca_notas),
             "dados": [self.format(biblioteca)["dados"] for biblioteca in biblioteca_notas] if biblioteca_notas else []
         }
+    
+    
+    def format_by_aluno(self, avalicoes_biblioteca):
+        if not avalicoes_biblioteca:
+            return {
+                "status": "sucesso",
+                "quantidade": 0,
+                "aluno": None,
+                "avaliacoes": []
+            }
+
+        aluno = avalicoes_biblioteca[0].aluno
+
+        return {
+            "status": "sucesso",
+            "quantidade": len(avalicoes_biblioteca),
+            "aluno": {
+                "id": aluno.id,
+                "nome": aluno.nome,
+                "sobrenome": aluno.sobrenome
+            },
+            "avaliacoes": [
+                {
+                    "id": biblioteca.id,
+                    "nota": biblioteca.nota
+                } for biblioteca in avalicoes_biblioteca
+            ]
+        }
+
         
