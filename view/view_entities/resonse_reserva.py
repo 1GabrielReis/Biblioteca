ee1ff49ea034a3f90e09354a22741dcb67e510de
gabriel <greis826@gmail.com>
2025-05-20 17:30:32 -0300

    criação da classe e inserção dos metodos basicos de resposta

---
+++ view/view_entities/resonse_reserva.py
@@ -0,0 +1,32 @@
+from ..response_base import Response_base
+
+class resonse_reserva(Response_base):
+    def format(self, reserva):
+        return {
+            "status": "sucesso",
+            "dados": {
+                "id": reserva.id,
+                "data_inicio": reserva.data_inicio,
+                "data_final": reserva.data_final,
+                "data_entregue": reserva.data_entregue,
+                "livro": {
+                    "id":reserva.livro.id,
+                    "titulo":reserva.livro.titulo,
+                    "autor": reserva.livro.autor,
+                    "editora": reserva.livro.editora
+                },
+                "aluno": {
+                    "id":reserva.aluno.id,
+                    "nome":reserva.aluno.nome,
+                    "sobrenome": reserva.aluno.sobrenome
+                }
+            }
+        }
+        
+
+    def format_list(self, reservas):
+        return {
+            "status": "sucesso",
+            "quantidade": len(reservas),
+            "dados": [self.format(reserva)["dados"] for reserva in reservas] if reservas else []
+        }
