b5aa5c741c895c413488dca13ecea5ed1788fdcb
gabriel <greis826@gmail.com>
2025-05-22 21:14:16 -0300

    base para classe response

---
+++ view/view_entities/response_avaliacao.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from ..response_base import Response_base
+
+class Response_avaliacao(Response_base):
+
+    def _formatDate(self, data: datetime):
+        pass
+
+    def format(self, avaliacao):
+        pass
+
+    def format_list(self, avaliacoes):
+        pass
+
+    def format_list_by_Aluno(self, avaliacoes):
+        pass
+
+    def format_list_by_livro(self, avaliacoes):
+        pass
+
+
+    def format_list_by_Aluno(self, avaliacoes):
+        pass
