06b6dd1a734a9f117092a4712c613db1dfdcda53
gabriel <greis826@gmail.com>
2025-05-26 01:40:58 -0300

    ajuste no importes, criação do metodo format_list_by_Aluno

--- view/view_entities/response_avaliacao.py
+++ view/view_entities/response_avaliacao.py
@@ -3,21 +3,20 @@ from datetime import datetime
 from ..response_base import Response_base
 from .response_aluno import Response_aluno
 from .response_livro import Response_livro
-from .response_reserva import Resonse_reserva
+from .response_reserva import Response_reserva
 
 class Response_avaliacao(Response_base):
     def __init__(self):
         super().__init__()
         self.response_aluno= Response_aluno()
         self.response_livro= Response_livro()
-        self.resonse_reserva= Resonse_reserva()
+        self.response_reserva= Response_reserva()
         
     def _format_reserva(self, reserva):
         reserva = reserva.copy()
         reserva['aluno']= reserva['aluno']['id']
         reserva['livro']= reserva['livro']['id']
         return reserva
-
 
     def _formatDate(self, data: datetime):
         if data is not None:
@@ -36,10 +35,10 @@ class Response_avaliacao(Response_base):
             "status": "sucesso",
             "dados":{
                 "id":avaliacao.id,
-                "nota":avaliacao.nota ,
+                "nota":avaliacao.nota,
                 "aluno": self.response_aluno.format(avaliacao.aluno)['dados'],
                 "livro":self.response_livro.format(avaliacao.livro)['dados'],
-                "reserva": self._format_reserva(self.resonse_reserva.format(avaliacao.reserva)['dados'])
+                "reserva": self._format_reserva(self.response_reserva.format(avaliacao.reserva)['dados'])
                 
             }
         }
@@ -52,11 +51,39 @@ class Response_avaliacao(Response_base):
         }
 
     def format_list_by_Aluno(self, avaliacoes):
-        pass
+        if not avaliacoes:
+            return {
+                "status": "sucesso",
+                "quantidade": 0,
+                "aluno": None,
+                "dados": []
+            }
+        
+        aluno = avaliacoes[0].aluno
+
+        return{
+            "status": "sucesso",
+            "aluno": {
+
+                "id": aluno.id,
+                "nome": aluno.nome,
+                "sobrenome": aluno.sobrenome
+            },
+            "quantidade": len(avaliacoes),
+            "dados": [
+                {
+                    "id": avaliacao.id,
+                    "nota": avaliacao.nota,
+                    "livro":self.response_livro.format(avaliacao.livro)['dados'],
+                    "reserva": self._format_reserva(self.response_reserva.format(avaliacao.reserva)['dados'])
+                } for avaliacao in avaliacoes
+            ]
+
+        }
 
     def format_list_by_livro(self, avaliacoes):
         pass
 
 
-    def format_list_by_Aluno(self, avaliacoes):
+    def format_list_by_Reserva(self, avaliacoes):
         pass
